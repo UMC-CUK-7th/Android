f9ca51f7f90045f00bf0f08a66f32c052b3c44f3
essp21 <essp0521@gmail.com>
2024-10-24 00:01:09 +0900

    3주차

---
+++ mission3/app/src/main/java/com/example/udemy_practice/AlbumFragment.kt
@@ -0,0 +1,42 @@
+package com.example.udemy_practice
+
+import MainActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.Toast
+import com.example.udemy_practice.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class AlbumFragment : Fragment() {
+    lateinit var binding : FragmentAlbumBinding
+
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater,container,false)
+
+        binding.albumAlbumIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm,HomeFragment())
+                .commitAllowingStateLoss()
+        }
+
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) {
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
+
+
+        return binding.root
+    }
+}
---
+++ mission3/app/src/main/java/com/example/udemy_practice/HomeFragment.kt
@@ -0,0 +1,44 @@
+package com.example.udemy_practice
+
+import MainActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.udemy_practice.databinding.FragmentHomeBinding
+import java.util.Timer
+import java.util.TimerTask
+
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homeAlbumImgIv1.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, AlbumFragment())
+                .commitAllowingStateLoss()
+        }
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+
+        return binding.root
+    }
+
+}
