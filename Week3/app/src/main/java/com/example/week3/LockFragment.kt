ee5c6e79d9754f32f12dc409bcfbf29fd9d0e557
EthanolSwab <y00000nje0ng@gmail.com>
2024-10-24 18:52:19 +0900

    feat: 3주차 과제

---
+++ Week3/app/src/main/java/com/example/week3/LockFragment.kt
@@ -0,0 +1,31 @@
+package com.example.week3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.week3.databinding.FragmentHomeBinding
+
+class LockFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homeAlbumImgIv1.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm , AlbumFragment())
+                .commitAllowingStateLoss()
+        }
+
+        return binding.root
+    }
+
+
+}
