daba0b583e528a9ed4646e19f18c70364b766ba9
essp21 <essp0521@gmail.com>
2024-10-24 18:29:29 +0900

    3주차

--- mission3/app/src/main/java/com/example/udemy_practice/SongFragment.kt
+++ mission3/app/src/main/java/com/example/udemy_practice/SongFragment.kt
@@ -13,15 +13,20 @@ class SongFragment : Fragment() {
     lateinit var binding: FragmentSongBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSongBinding.inflate(inflater,container,false)
+        binding = FragmentSongBinding.inflate(inflater, container, false)
+        binding.songMixoffTg.setOnClickListener {
+            binding.songMixoffTg.visibility = View.GONE
+            binding.songMixonTg.visibility = View.VISIBLE
+        }
+
+        binding.songMixonTg.setOnClickListener {
+            binding.songMixoffTg.visibility = View.VISIBLE
+            binding.songMixonTg.visibility = View.GONE
+        }
 
         return binding.root
     }
-
-
-
 }
